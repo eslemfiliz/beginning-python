6f34d693074a4ba5387c60a814754e405a5d2985
eslemfiliz <138863865+eslemfiliz@users.noreply.github.com>
2024-03-15 22:42:55 +0300

    git push

---
+++ Örnek Algoritmalar/input_alma.py
@@ -0,0 +1,25 @@
+# a = input("Lütfen bir sayı giriniz:")
+# print("Kullanıcının girdiği değer",a)
+
+# a = int(input("Lütfen bir sayı giriniz:"))
+# print(a*3)
+
+# a = input("Lütfen bir sayı giriniz:")
+# print(a*3)
+
+# a = input("Lütfen birinci sayıyı giriniz:")
+# b = input("Lütfen ikinci sayıyı giriniz:")
+# c = input("Lütfen üçüncü sayıyı giriniz:")
+# print("Toplamları", a+b+c)
+
+# a = int(input("Lütfen birinci sayıyı giriniz:"))
+# b =  int(input("Lütfen ikinci sayıyı giriniz:"))
+# c = int(input("Lütfen üçüncü sayıyı giriniz:"))
+# print("Toplamları", a+b+c)
+
+try:
+    a = a = int(input("a:"))
+    print(a)
+except ValueError:
+    print("Lütfen doğru formatta input girin.")
+        
