d250700ad0c3f8f0919795f6f5783bf060e57039
eslemfiliz <138863865+eslemfiliz@users.noreply.github.com>
2024-03-30 21:22:41 +0300

    git push

--- Döngü Yapılarını Kullanma/for_dögüleri.py
+++ Döngü Yapılarını Kullanma/for_dögüleri.py
@@ -1,13 +1,22 @@
-# liste = [1,2,3,4,5]
+# # liste = [1,2,3,4,5]
+
+# # for eleman in liste:
+# #     print(eleman)
+
+# toplam = 0
+
+# liste= [1,2,3]
 
 # for eleman in liste:
-#     print(eleman)
+#     toplam = toplam + eleman
+#     print("Toplam {}: Eleman :{}".format(toplam,eleman)) 
+# print(toplam)    
 
-toplam = 0
-
-liste= [1,2,3]
-
-for eleman in liste:
-    toplam = toplam + eleman
-    print("Toplam {}: Eleman :{}".format(toplam,eleman)) 
-print(toplam)    
+liste = [2,1,10,2,23,1,56,3]
+ 
+total = 0
+for i in liste:
+    if (not (i % 2 == 0)):
+        total += i
+ 
+print(total)
